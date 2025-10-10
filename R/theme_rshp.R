7f6dab9038eb7c062d4d33e4f06d8c3ff2f42072
matthh10 <matthew.hoyle@phs.scot>
2025-10-10 15:47:16 +0000

    add theme_rshp function

--- R/theme_rshp.R
+++ R/theme_rshp.R
@@ -2,28 +2,33 @@
 
 #' Function to apply the PHS RSHP team's theme to a ggplot2 object
 #'
-#' @param ...
+#' @param ... Extra ggplot2::theme() options
 #' @param base_size Base font size as numeric
-#'
-#' @return
+#' @importFrom ggplot2 %+replace%
+#' @return ggplot object with RSHP theming
 #' @export
 #'
 #' @examples
 #'
+#' iris |>
+#'   ggplot2::ggplot(ggplot2::aes(Sepal.Length, Sepal.Width)) +
+#'   ggplot2::geom_point() +
+#'   theme_rshp()
 #'
+
 theme_rshp <- function(..., base_size = 11){
-  theme_grey() %+replace%
-    theme(
-      text = element_text(size = base_size,  family = "sans"),
-      axis.title = element_text(colour="#3F3685"),
-      axis.title.x = element_text(margin = margin(0.1, 0, 0, 0, unit = "cm"), size = rel(1.1)),
-      axis.title.y = element_text(margin = margin(0, 0.1, 0, 0, unit = "cm"), angle =90,  size = rel(1.1)),
-      legend.title = element_text(colour = "#3F3685"),
+  ggplot2::theme_grey() %+replace%
+    ggplot2::theme(
+      text = ggplot2::element_text(size = base_size,  family = "sans"),
+      axis.title = ggplot2::element_text(colour="#3F3685"),
+      axis.title.x = ggplot2::element_text(margin = ggplot2::margin(0.1, 0, 0, 0, unit = "cm"), size = ggplot2::rel(1.1)),
+      axis.title.y = ggplot2::element_text(margin = ggplot2::margin(0, 0.1, 0, 0, unit = "cm"), angle =90,  size = ggplot2::rel(1.1)),
+      legend.title = ggplot2::element_text(colour = "#3F3685"),
       legend.position = "right",
       legend.direction = "vertical",
-      plot.caption = element_text(size = rel(0.75), hjust = 1),
-      panel.background = element_blank(),
-      axis.line.x = element_line(colour = "black"),
-      axis.line.y = element_line(colour = "black")
+      plot.caption = ggplot2::element_text(size = ggplot2::rel(0.75), hjust = 1),
+      panel.background = ggplot2::element_blank(),
+      axis.line.x = ggplot2::element_line(colour = "black"),
+      axis.line.y = ggplot2::element_line(colour = "black")
     )
 }
