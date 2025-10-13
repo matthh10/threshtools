3a34a74c5d94de7056ac2780fa289f77c35a4aac
matthh10 <matthew.hoyle@phs.scot>
2025-10-13 14:05:40 +0000

    Add angle_x to theme_rshp

--- R/theme_rshp.R
+++ R/theme_rshp.R
@@ -3,7 +3,9 @@
 #' Function to apply the PHS RSHP team's theme to a ggplot2 object
 #'
 #' @param ... Extra ggplot2::theme() options
+#' @param angle_x Boolean operator (default = FALSE) controls whether x axis text should be angled
 #' @param base_size Base font size as numeric
+#'
 #' @importFrom ggplot2 %+replace%
 #' @return ggplot object with RSHP theming
 #' @export
@@ -16,19 +18,43 @@
 #'   theme_rshp()
 #'
 
-theme_rshp <- function(..., base_size = 11){
-  ggplot2::theme_grey() %+replace%
-    ggplot2::theme(
-      text = ggplot2::element_text(size = base_size,  family = "sans"),
-      axis.title = ggplot2::element_text(colour="#3F3685"),
-      axis.title.x = ggplot2::element_text(margin = ggplot2::margin(0.1, 0, 0, 0, unit = "cm"), size = ggplot2::rel(1.1)),
-      axis.title.y = ggplot2::element_text(margin = ggplot2::margin(0, 0.1, 0, 0, unit = "cm"), angle =90,  size = ggplot2::rel(1.1)),
-      legend.title = ggplot2::element_text(colour = "#3F3685"),
-      legend.position = "right",
-      legend.direction = "vertical",
-      plot.caption = ggplot2::element_text(size = ggplot2::rel(0.75), hjust = 1),
-      panel.background = ggplot2::element_blank(),
-      axis.line.x = ggplot2::element_line(colour = "black"),
-      axis.line.y = ggplot2::element_line(colour = "black")
-    )
+theme_rshp <- function(..., base_size = 11, angle_x = FALSE){
+
+  if (angle_x == TRUE){
+
+    ggplot2::theme_grey() %+replace%
+      ggplot2::theme(
+        text = ggplot2::element_text(size = base_size,  family = "sans"),
+        axis.title = ggplot2::element_text(colour="#3F3685"),
+        axis.title.x = ggplot2::element_text(margin = ggplot2::margin(0.1, 0, 0, 0, unit = "cm"), size = ggplot2::rel(1.1)),
+        axis.title.y = ggplot2::element_text(margin = ggplot2::margin(0, 0.1, 0, 0, unit = "cm"), angle =90,  size = ggplot2::rel(1.1)),
+        legend.title = ggplot2::element_text(colour = "#3F3685"),
+        legend.position = "right",
+        legend.direction = "vertical",
+        plot.caption = ggplot2::element_text(size = ggplot2::rel(0.75), hjust = 1),
+        panel.background = ggplot2::element_blank(),
+        axis.line.x = ggplot2::element_line(colour = "black"),
+        axis.line.y = ggplot2::element_line(colour = "black"),
+        axis.text.x = ggplot2::element_text(angle = 45, vjust = 1, hjust = 1)
+      )
+
+  } else {
+
+    ggplot2::theme_grey() %+replace%
+      ggplot2::theme(
+        text = ggplot2::element_text(size = base_size,  family = "sans"),
+        axis.title = ggplot2::element_text(colour="#3F3685"),
+        axis.title.x = ggplot2::element_text(margin = ggplot2::margin(0.1, 0, 0, 0, unit = "cm"), size = ggplot2::rel(1.1)),
+        axis.title.y = ggplot2::element_text(margin = ggplot2::margin(0, 0.1, 0, 0, unit = "cm"), angle =90,  size = ggplot2::rel(1.1)),
+        legend.title = ggplot2::element_text(colour = "#3F3685"),
+        legend.position = "right",
+        legend.direction = "vertical",
+        plot.caption = ggplot2::element_text(size = ggplot2::rel(0.75), hjust = 1),
+        panel.background = ggplot2::element_blank(),
+        axis.line.x = ggplot2::element_line(colour = "black"),
+        axis.line.y = ggplot2::element_line(colour = "black")
+      )
+
+  }
+
 }
